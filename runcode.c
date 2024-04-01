050b34c308079d09990fc5962665be49692f067b
pohxiangah <yeohpohxiang@gmail.com>
2024-04-01 23:01:18 +0800

    update

--- runcode.c
+++ runcode.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main(){
-    remove ("systeman");
-    system("gcc main.c systemadmin.c pgadmin.c students.c profilemanage.c lecturer.c -o systeman");
+    remove ("start");
+    system("gcc main.c systemadmin.c pgadmin.c students.c profilemanage.c lecturer.c -o start");
 }
 
 
